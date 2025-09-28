c02b77528b2f622dd619a1a3d67ff954b22b36ed
FreeBird-spectre <sethabir699@gmail.com>
2025-09-28 11:03:50 +0530

    Update Constructor Outside Class.cpp

--- Constructor Outside Class.cpp
+++ Constructor Outside Class.cpp
@@ -10,17 +10,20 @@ class Student {
     double fee;
 
 public:
-    Student() {
-        cout << "Enter the name of Student: ";
-        cin >> name;
-        cout << "Enter the fee: ";
-        cin >> fee;
-    }
-
-    void display() {
-        cout << "Student Name: " << name << "\nFee: " << fee << endl;
-    }
+    Student();
+    void display();
 };
+
+Student::Student() {
+    cout << "Enter the name of Student: ";
+    cin >> name;
+    cout << "Enter the fee: ";
+    cin >> fee;
+}
+
+void Student::display() {
+    cout << "Student Name: " << name << "\nFee: " << fee << endl;
+}
 
 int main() {
     Student s;
@@ -28,10 +31,10 @@ int main() {
     return 0;
 }
 
-Enter the name of Student: abir 
-Enter the fee: 68999
-Student Name: abir
-Fee: 68999
+Enter the name of Student: AbirSeth
+Enter the fee: 200000
+Student Name: AbirSeth
+Fee: 200000
 
 
 === Code Execution Successful ===
