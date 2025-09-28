6bbe6df42f29f676252b2665c631341bcd1a5576
FreeBird-spectre <sethabir699@gmail.com>
2025-09-28 10:40:28 +0530

    Create Constructor Inside Class.cpp

---
+++ Constructor Inside Class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    double fee;
+
+public:
+    Student() {
+        cout << "Enter the name of Student: ";
+        cin >> name;
+        cout << "Enter the fee: ";
+        cin >> fee;
+    }
+
+    void display() {
+        cout << "\nStudent Name: " << name;
+        cout << "\nFee: " << fee << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.display();
+    return 0;
+}
+
+Enter the name of Student: Abir 
+Enter the fee: 65900
+
+Student Name: Abir
+Fee: 65900
+
+
+=== Code Execution Successful ===
