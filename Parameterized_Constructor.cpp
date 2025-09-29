562bc0278d60c9fc8d6bf8f88b8776e39e8a061b
FreeBird-spectre <sethabir699@gmail.com>
2025-09-29 23:18:34 +0530

    Create Parameterized_Constructor.cpp

---
+++ Parameterized_Constructor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Date {
+public:
+    int day, month, year;
+
+    Date(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+
+    void show() {
+        cout << "Today's date is: " << day << "/" << month << "/" << year << endl;
+    }
+};
+
+int main() {
+    int d, m, y;
+
+    cout << "Enter day: ";
+    cin >> d;
+    cout << "Enter month: ";
+    cin >> m;
+    cout << "Enter year: ";
+    cin >> y;
+
+    Date today(d, m, y);
+
+    today.show();
+
+    return 0;
+}
+
+Enter day: 5
+Enter month: 11
+Enter year: 2020
+Today's date is: 5/11/2020
+
+
+=== Code Execution Successful ===
